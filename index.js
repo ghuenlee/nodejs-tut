c8fd54f1bc19c42c54f2e9a47d9f608a281bfe56
Ghuen Lee <kylesentient@gmail.com>
2016-03-12 22:20:29 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+// app.get('/', function(req, res){
+//   res.send('Connection established');
+// });
+
+var port = 3000;
+
+app.get('/about', function(req,res){
+  res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public/'));
+
+app.listen(port, function (){
+  console.log('Express server started on port: '+port);
+});
